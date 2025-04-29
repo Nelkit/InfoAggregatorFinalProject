b5d48f9c19f522b9ec1e9dac9a0abcd66fd911b9
Santiago Cardenas <scardenasc22@gmail.com>
2025-04-29 17:14:54 +1000

    nyt webscrapping and barplot of sources

--- aggregator/visualizer.py
+++ aggregator/visualizer.py
@@ -1,6 +1,9 @@
 import plotly.express as px
 from datetime import datetime
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from entities.news_article import NewsArticle
 
 class NewsVisualizer:
     #TODO Implementar los siguientes graficos
@@ -10,9 +13,24 @@ class NewsVisualizer:
     # 4. Numero de palabras por artículo | Luis
 
     # 1. Gráfico de distribución por fuente
-    def source_distribution_plot(self, articles):
-        df = [{"source": "source1", "count": 10}, {"source": "source2", "count": 20}]
-        fig = px.bar(df, x='source', title="Distribution by Source")
+    def source_distribution_plot(self, articles : list[NewsArticle]):
+        sources = [a.source for a in articles]
+        source_counter = {}
+        for s in sources:
+            if s in source_counter.keys():
+                source_counter[s] += 1
+            else:
+                source_counter[s] = 1
+        df = pd.DataFrame(data = zip(source_counter.keys(), source_counter.values()), columns = ['source', 'total articles']).sort_values(by = 'total articles', ascending=False)
+        fig, _ = plt.subplots(figsize = (10, 8))
+        sns.despine(fig)
+        sns.barplot(
+            data = df,
+            y = 'source',
+            x = 'total articles',
+            hue = 'source',
+            palette = 'Blues_r'
+        )
         return fig
 
     # 2. Nube de Palabras por noticia
